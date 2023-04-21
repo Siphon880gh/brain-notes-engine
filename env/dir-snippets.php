2f0c61ed6ec84a05720d5e3fcf9ced7f27a18090
Siphon880gh <weng.f.fung@gmail.com>
2023-04-20 21:55:02 -0700

    Hybridized the code so it can check Obsidian vault when localhost but ./curriculum when on server which has the pipeline fo fetching the Obsidian repository

--- env/dir-snippets.php
+++ env/dir-snippets.php
@@ -1,8 +1,27 @@
 <?php
+// DIR_SNIPPETS is the folder of MD files and JSON that is made up of the curriculum of code snippets, guides and tutorials.
+// I like locally authoring on Obsidian Editor (great for MD files) but the code needs to exist on another folder path.
 
-
-$DIR_SNIPPETS = "curriculum/";
-// TODO: Detect production or local development
+// Therefore, I must detect production or local development
 // Local development should use Obsidian vault directory at the author's computer
-// Production environment should use ./curriculum/ which will be ignored at .gitignore but SSH will fetch and reset a sub repository at ./curriculum/ (not pull because we want automatic scripts without the possibility of needing supervision for merge conflicts)
+// Production environment should use ./curriculum/ which will be ignored at .gitignore but SSH will fetch and reset a sub 
+// repository at ./curriculum/ (not pull because we want automatic scripts without the possibility of needing supervision 
+// for merge conflicts)
+
+$DIR_SNIPPETS = "curriculum/"; // default
+
+$host = $_SERVER['HTTP_HOST'];
+
+if (strpos($host, 'localhost') !== false) {
+    // When viewing app in localhots, render MD notes from Obsidian Vault
+    $DIR_SNIPPETS = "/Users/wengffung/Google Drive/My Drive/Obsidian MD/Document Vaults/Dev/curriculum/"; 
+
+} elseif (strpos($host, 'wengindustry.com') !== false) {
+    // Is fine but kept here for scalability
+} else {
+    // Is fine but kept here for scalability
+}
+
+// die($DIR_SNIPPETS);
+
 ?>
