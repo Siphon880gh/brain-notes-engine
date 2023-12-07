7ddb62a466197c39f7d5960fcc5f31e276a662ad
Siphon880gh <weng.f.fung@gmail.com>
2023-12-06 23:12:25 -0800

    More to description

--- assets/js/index.js
+++ assets/js/index.js
@@ -220,18 +220,19 @@ function htmlTableOfContents(tocEl, markdownContentEl) {
 // Visiting from https://wengindustry.com/devbrain ?? Then rebrand
 if(window.location.href.includes("devbrain")) {
 
+    $("title").text("Developer Brain")
     $("h1.title").text("Developer Brain")
     
     $(".site-header summary small").html(`<span>Read more...</span>
-    <p>
-    Everything on coding - web development, mobile development, data science, AI and related fields - in bite size guides, code snippet and references. All here in one place for programmers of all levels.
+    <p class="container" style="padding-left:30px; padding-right:30px;">
+    Everything on coding - web development, mobile development, game development, data science, AI and related fields - in bite size guides, code snippet and references. All here in one place for programmers of all levels.
     </p>
     
     `)
     
     $(".site-header details .container .card").html(`
     <p>
-        <a target="_blank" href="https://wengindustry.com">Weng<a/> will keep adding to this knowledge brain as he tries to become a coder of multiple tech fields finding new opportunities as a tech enterpreneur
+        <a target="_blank" href="https://wengindustry.com">Weng<a/> will keep adding to this knowledge brain because he wants to know all the tech fields in order to find new opportunities as an enterpreneur.
     </p>
     
     `)
