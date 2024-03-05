04c5b5c2a9178aa5fc2252486cb107a27fd6d236
Siphon880gh <weng.f.fung@gmail.com>
2024-03-05 02:32:55 -0800

    User flows corrected

--- env/templates-bizbrain/description-must.php
+++ env/templates-bizbrain/description-must.php
@@ -1,6 +1,6 @@
 <div class="description-must">
     <center>
-        Did you mean to check out my <a target="_blank" href="../devbrain">software development / programming / coding notes</a>? 
+        Did you mean to check out my <a target="_blank" href="../devbrain">software development / programming / coding notes</a>?<br/> 
         Or my <a target="_blank" href="../3dbrain">3d modeling / animation / video game development notes</a>?
     </center>
 </div>
--- env/templates-devbrain/description-must.php
+++ env/templates-devbrain/description-must.php
@@ -1,6 +1,6 @@
 <div class="description-must">
     <center>
-        Did you mean to check out my <a target="_blank" href="../bizbrain">tech startup business notes</a>? 
+        Did you mean to check out my <a target="_blank" href="../bizbrain">tech startup business notes</a>?<br/>
         Or my <a target="_blank" href="../3dbrain">3d modeling / animation / video game development notes</a>?
     </center>
 </div>
