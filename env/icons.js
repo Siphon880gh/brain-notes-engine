3ffd4c25fb4c9d7bf478390e704b8c714ff64219
Siphon880gh <weng.f.fung@gmail.com>
2024-08-24 19:14:25 -0700

    DevOps expanded to include all enterprise tools - 2

--- env/icons.js
+++ env/icons.js
@@ -1,13 +1,13 @@
 var icons = {
-    "AI Developer": "🤖",
-    "AI Engineer": "🤖",
-    "AWS Cloud": "☁️",
-    "Databases - MySQL, NoSQL, and More!": "🗃",
-    "Data Structures, Algorithms, Architechtures, Systems Design": "Ω",
-    "DevOps": "⚙️",
-    "iOS": "📱",
-    "Version Control - git, github, gitlab": "♽",
-    "Plug-in Development": "🔌",
-    "Web Development": "🌐",
-    "Web Development - Rapid Development": "🌐"
-  }
+  "AI Developer": "🤖",
+  "AI Engineer": "🤖",
+  "AWS Cloud": "☁️",
+  "Databases - MySQL, NoSQL, and More!": "🗃",
+  "Data Structures, Algorithms, Architechtures, Systems Design": "Ω",
+  "Enterprise - DevOps, Server Admin, SaaS Scaling, Payment Systems": "🌌",
+  "iOS": "📱",
+  "Version Control - git, github, gitlab": "♽",
+  "Plug-in Development": "🔌",
+  "Web Development": "🌐",
+  "Web Development - Rapid Development": "🌐"
+}
