8884bf84c51fe12be2e812696859dbe2b081143d
Siphon880gh <weng.f.fung@gmail.com>
2025-02-06 21:01:17 -0800

    Updated healthbrain disclaimer

--- env/templates-healthbrain/description-must.php
+++ env/templates-healthbrain/description-must.php
@@ -2,8 +2,8 @@
     $disclaimer = "Please note that the health information provided here is for educational purposes only and is not intended to be used for diagnosis or treatment of any medical condition. This information should not be used as a substitute for professional medical advice, diagnosis, or treatment. Always seek the advice of your physician or other qualified health provider with any questions you may have regarding a medical condition. Never disregard professional medical advice or delay in seeking it because of something you have read here.";
 ?>
 <div class="description-must" style="position:relative; overflow:visible;">
-    <div style="width:100%; text-align:center; line-height: 80%; font-size: 80%;">
-        <b onclick="alert('<?php echo $disclaimer;?>')">Disclaimer: Please note that the health information provided here is for educational purposes only.. By reading the information, you agree to this and the rest of the disclaimer (click to expand disclaimer).</b> 
+    <div style="width:100%; text-align:center; font-size: 80%;">
+        <b onclick="alert('<?php echo $disclaimer;?>')">Disclaimer: Please note that the health information provided here is for educational purposes only..<br/>By reading the information, you agree to this and the rest of the disclaimer (click to expand disclaimer).</b> 
     </div>
 
 </div>
